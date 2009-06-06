1d961068d476eb9b0e1fbf260c08f3827918a090
Shawn Sulma <shawn@470th.org>
2009-06-05 22:04:37 -0400

    some cleanup, documentation additions

--- genoshatest/xmltest.py
+++ genoshatest/xmltest.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#    genoshatest.py - test cases for Genosha over JSON
+#    genoshatest/xmltest.py - test cases for Genosha over XML
 #    Copyright (C) 2009 Shawn Sulma <genosha@470th.org>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -19,7 +19,7 @@ import time, sys, unittest
 from genosha.XML import dumps, loads
 import genoshatest
 
-class GenoshaJSONTests ( genoshatest.GenoshaTests ) :
+class GenoshaXMLTests ( genoshatest.GenoshaTests ) :
     def setUp ( self ) :
         self.marshal = dumps
         self.unmarshal = loads
@@ -27,7 +27,4 @@ class GenoshaJSONTests ( genoshatest.GenoshaTests ) :
         self.unicode = unicode
 
 if __name__ == "__main__":
-    if '+gc' in sys.argv :
-        del sys.argv[sys.argv.index('+gc')]
-        genosha.USE_GC_REDUCTION = True
     unittest.main()
